726f75cee41be5c08d6d385f5ca565526bf70135
Renee Baecker <cpan@perl-services.de>
2007-12-01 13:44:34 +0000

    Import of RENEEB/Class-Superclasses-0.03 from CPAN.
    gitpan-cpan-distribution: Class-Superclasses
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         RENEEB/Class-Superclasses-0.03.tar.gz
    gitpan-cpan-author:       RENEEB
    gitpan-cpan-maturity:     released

--- lib/Class/Superclasses.pm
+++ lib/Class/Superclasses.pm
@@ -4,106 +4,143 @@ use strict;
 use warnings;
 use PPI;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 sub new{
-  my ($class,$doc) = @_,
-  my $self         = {};
-  bless $self,$class;
-  
-  $self->document($doc);
-  
-  return $self;
+    my ($class,$doc) = @_,
+    my $self         = {};
+    bless $self,$class;
+    
+    $self->document($doc);
+    
+    return $self;
 }# new
 
 sub superclasses{
-  my ($self) = @_;    
-  return wantarray ? @{$self->{super}} : $self->{super};
+    my ($self) = @_;    
+    return wantarray ? @{$self->{super}} : $self->{super};
 }# superclasses
 
 sub document{
-  my ($self,$doc) = @_;
-  if(defined $doc){
-    $self->{document} = $doc;
-    $self->{super}    = $self->_find_super($doc);
-  }
+    my ($self,$doc) = @_;
+    if(defined $doc){
+        $self->{document} = $doc;
+        $self->{super}    = $self->_find_super($doc);
+    }
 }# document
 
 sub _find_super{
-  my ($self,$doc) = @_;
-  my $ppi    = PPI::Document->new($doc) or die $!;
-  
-  my $varref = $ppi->find('PPI::Statement::Variable');
-  my @vars   = ();
-  if($varref){
-    @vars    = $self->_get_isa_values($varref);
-  }
-  
-  my $baseref = $ppi->find('PPI::Statement::Include');
-  my @base    = ();
-  if($baseref){
-    @base = $self->_get_base_values([grep{$_->module eq 'base'}@$baseref]);
-  }
-  return [@vars,@base];
+    my ($self,$doc) = @_;
+    my $ppi    = PPI::Document->new($doc) or die $!;
+    
+    my $varref = $ppi->find('PPI::Statement::Variable');
+    my @vars   = ();
+    if($varref){
+        @vars    = $self->_get_isa_values($varref);
+    }
+    
+    my $baseref  = $ppi->find('PPI::Statement::Include');
+    my @base     = ();
+    my @includes = qw(base parent);
+    if($baseref){
+        @base = $self->_get_include_values([grep{my $i = $_->module; grep{ $_ eq $i }@includes }@$baseref]);
+    }
+    return [@vars,@base];
 } # _find_super
 
-sub _get_base_values{
-  my ($self,$baseref) = @_;
-  my @parents;
-  for my $base(@$baseref){
-    if($base->find_any('PPI::Statement::Expression')){
-      push(@parents,$self->_parse_expression($base));
+sub _get_include_values{
+    my ($self,$baseref) = @_;
+    my @parents;
+    for my $base(@$baseref){
+        my @tmp_array;
+        if( $base->find_any('PPI::Statement::Expression') ){
+            push(@parents,$self->_parse_expression( $base ));
+        }
+        elsif( $base->find_any('PPI::Token::QuoteLike::Words') ){
+            push(@parents,$self->_parse_quotelike( $base ));
+        }
+        elsif( $base->find( \&_any_quotes ) ){
+            push @parents,$self->_parse_quotes( $base );
+        }
+        @tmp_array = grep{ $_ ne '-norequire' }@tmp_array if $base->module eq 'parent';
+        push @parents, @tmp_array;
     }
-    elsif($base->find_any('PPI::Token::QuoteLike::Words')){
-      push(@parents,$self->_parse_quotelike($base));
-    }
-  }
-  return @parents;
+    return @parents;
 }# _get_base_values
 
-sub _get_isa_values{
-  my ($self,$varref) = @_;
-  my @parents;
-  for my $variable(@$varref){
-    my @children = $variable->children();
-    #print Dumper($variable);
+sub _any_quotes{
+    my ($parent,$elem) = @_;
     
-    if(grep{$_->content() eq '@ISA'}@children){
-      if($variable->find_any('PPI::Statement::Expression')){
-        push(@parents,$self->_parse_expression($variable));
-      }
-      elsif($variable->find_any('PPI::Token::QuoteLike::Words')){
-        push(@parents,$self->_parse_quotelike($variable));
-      }
+    $parent == $elem->parent and (
+        $elem->isa( 'PPI::Token::Quote::Double' ) or
+        $elem->isa( 'PPI::Token::Quote::Single' )
+    );
+}
+
+sub _get_isa_values{
+    my ($self,$varref) = @_;
+    my @parents;
+    for my $variable(@$varref){
+        my @children = $variable->children();
+        #print Dumper($variable);
+        
+        if(grep{$_->content() eq '@ISA'}@children){
+            if($variable->find_any('PPI::Statement::Expression')){
+                push(@parents,$self->_parse_expression($variable));
+            }
+            elsif($variable->find_any('PPI::Token::QuoteLike::Words')){
+                push(@parents,$self->_parse_quotelike($variable));
+            }
+        }
     }
-  }
-  return @parents;
+    return @parents;
 }# _get_values
 
 sub _parse_expression{
-  my ($self,$variable) = @_;
-  my $ref = $variable->find('PPI::Statement::Expression');
-  my @parents;
-  for my $element($ref->[0]->children()){
-    if($element->class() =~ /^PPI::Token::Quote::/){
-      my $separator = $element->{seperator};
-      (my $value = $element->content()) =~ s~\Q$separator\E(.*)\Q$separator\E~$1~;
-      push(@parents,$value);
+    my ($self,$variable) = @_;
+    my $ref = $variable->find('PPI::Statement::Expression');
+    my @parents;
+    for my $element($ref->[0]->children()){
+        if($element->class() =~ /^PPI::Token::Quote::/){
+            my $separator = $element->{seperator};
+            (my $value = $element->content()) =~ s~\Q$separator\E(.*)\Q$separator\E~$1~;
+            push(@parents,$value);
+        }
     }
-  }
-  return @parents;
+    return @parents;
 }# _parse_expression
 
+sub _parse_quotes{
+    my ($self,$variable,$type) = @_;
+    
+    my %map = (
+        Single => q~'~,
+        Double => q~"~,
+    );
+    
+    my @parents;
+    
+    for my $element( $variable->children ){
+        my ($type) = ref($element) =~ /PPI::Token::Quote::([^:]+)$/;
+        next unless $type;
+        my $value  = $element->content;
+        $value =~ s/$map{$type}//g;
+        push @parents, $value;
+    }
+
+    return @parents;
+}
+
 sub _parse_quotelike{
-  my ($self,$variable) = @_;
-  my $words        = ($variable->find('PPI::Token::QuoteLike::Words'))[0]->[0];
-  my $operator     = $words->{operator};
-  my $section_type = $words->{sections}->[0]->{type};
-  my ($left,$right) = split(//,$section_type);
-  $right = $left unless defined $right;
-  (my $value = $words->content()) =~ s~$operator\Q$left\E(.*)\Q$right\E~$1~;
-  my @parents = split(/\s+/,$value);
-  return @parents;
+    my ($self,$variable) = @_;
+    my $words         = ($variable->find('PPI::Token::QuoteLike::Words'))[0]->[0];
+    my $operator      = $words->{operator};
+    my $section_type  = $words->{sections}->[0]->{type};
+    my ($left,$right) = split(//,$section_type);
+    $right            = $left unless defined $right;
+    (my $value        = $words->content()) =~ s~$operator\Q$left\E(.*)\Q$right\E~$1~;
+    my @parents       = split(/\s+/,$value);
+    return @parents;
 }# _parse_quotelike
 
 
